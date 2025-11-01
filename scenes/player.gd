1ae13ae1c7a9117d596b1104f9231964cb7bd6ce
tn1207 <tn1207@g.rit.edu>
2025-11-01 17:35:33 -0400

    Character's Ability to Walk Around
    Player Character can safely run and walk around map. Implemented in player.gd:_process:

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,18 +2,38 @@ extends CharacterBody2D
 
 @export var move_speed : float = 100
 @export var run_speed : float = 150
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _process(_delta):
 	#Get input direction
 	var toggle_run = false
-	var animated_sprite
 	var input_direction = Vector2(
 		Input.get_axis("left", "right"),
 		Input.get_axis("up", "down")
 		)
-		
-		
-	print(input_direction)
+	if input_direction.x == 0 && input_direction.y == 0 :
+		animated_sprite.play("idle")
+	if input_direction.x > 0 && input_direction.y == 0 :
+		animated_sprite.flip_h = false
+		animated_sprite.play("walk_horizontal")
+	if input_direction.x < 0 && input_direction.y == 0 :
+		animated_sprite.flip_h = true
+		animated_sprite.play("walk_horizontal")
+	if input_direction.x == 0 && input_direction.y > 0 :
+		animated_sprite.play("walk_down")
+	if input_direction.x == 0 && input_direction.y < 0 :
+		animated_sprite.play("walk_up")
+	if input_direction.x == 0 && input_direction.y < 0 && toggle_run == true :
+		animated_sprite.play("run_up")
+	if input_direction.x == 0 && input_direction.y > 0 && toggle_run == true :
+		animated_sprite.play("run_down")
+	if input_direction.x < 0 && input_direction.y == 0 && toggle_run == true :
+		animated_sprite.flip_h = true
+		animated_sprite.play("run_horizontal")
+	if input_direction.x > 0 && input_direction.y == 0 && toggle_run == true :
+		animated_sprite.flip_h = false
+		animated_sprite.play("run_horizontal")
+	
 	 
 	
 	#Update velocity
