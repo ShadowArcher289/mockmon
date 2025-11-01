d10a3b8491cf6b854d9f374bbc43aecfbd86b732
ShadowArcher289 <ahasuncion21@gmail.com>
2025-11-01 14:01:10 -0400

    Added move types

---
+++ scripts/MoveType.gd
@@ -0,0 +1,16 @@
+extends Object
+class_name move
+
+var type : String; 
+var atk_damage : int = 0;
+var category : String;
+var pp : int;
+var accuracy : int; # > 100 is always hits
+
+func _init(input_type: String, input_atk_damage: int, input_category: String, input_pp: int, input_accuracy: int) -> void:
+	self.type = input_type;
+	self.atk_damage = input_atk_damage;
+	self.category = input_category;
+	self.pp = input_pp;
+	self.accuracy = input_accuracy;
+	
--- scripts/npc_trainer.gd
+++ scripts/npc_trainer.gd
@@ -3,3 +3,9 @@ extends Node2D
 @export var mockmon_party: Array[Node2D] = [];
 
 @export var current_mockmon: Node2D;
+
+func _ready() -> void:
+	pass;
+
+func _process(delta: float) -> void:
+	pass;
