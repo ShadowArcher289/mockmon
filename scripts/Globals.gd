a62f281df9e543997a72b9fbdea5019914c0fafd
ShadowArcher289 <ahasuncion21@gmail.com>
2025-11-01 19:01:46 -0400

    implemented battle

--- scripts/Globals.gd
+++ scripts/Globals.gd
@@ -3,5 +3,5 @@ extends Node
 const LEVEL = 50.0;
 
 func calculate_damage(enemy_atk: float, my_defense: float, move_power: float, move_type: String, weaknesses: Array[String], resistances: Array[String]) -> int:
-	var type_modifier = is_weak(move_type: String, weaknesses: Array[String], resistances: Array[String]);
+	#var type_modifier = is_weak(move_type: String, weaknesses: Array[String], resistances: Array[String]);
 	return ceili( ( ((((2.0*LEVEL)/5)+2) * move_power * (enemy_atk/my_defense)) /50.0) + 2);
--- scripts/SignalBus.gd
+++ scripts/SignalBus.gd
@@ -1,4 +1,7 @@
 extends Node
 
-signal player_moved;
-signal npc_moved;
+signal player_move_finished;
+signal npc_move_finished;
+
+signal player_done_with_battle;
+signal npc_done_with_battle;
