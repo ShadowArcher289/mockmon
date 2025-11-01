6c53720250442afc06dab03dcab6b2e97f2fd016
ShadowArcher289 <ahasuncion21@gmail.com>
2025-11-01 15:59:28 -0400

    Working on damage calculation

--- scripts/dripped_out_rat.gd
+++ scripts/dripped_out_rat.gd
@@ -4,6 +4,26 @@ var sick_burn = move.new("Fire", 30, "Special", 20, 100);
 #var fling_drip = move.new("Steel", 40, "Physical", 10, 90);
 #var fling_drip = move.new("Steel", 40, "Physical", 10, 90);
 
+var moves: Array[move] = [];
+
+const MAX_HP = 120; # base sats
+const BASE_ATK = 10;
+const BASE_DEF = 20;
+const BASE_SPEC_ATK = 30;
+const BASE_SPEC_DEF = 30;
+const BASE_SPEED = 50;
+
+var currentHp = MAX_HP;
 
 func _ready() -> void:
-	pass;
+	moves = [fling_drip, sick_burn];
+
+func use_move(move_number: int, target: Node2D): ## Use a move on a given target given the move number
+	if target.has_method("take_damage"):
+		target.take_damage(moves[move_number-1]);
+
+func get_move(move_number: int): ## Returns the mockmon's move
+	return moves[move_number-1];
+
+func take_damage(move_used: move):
+	
