8432a0a68bc86d55f4c8a45e861a9870da7b15b0
ShadowArcher289 <ahasuncion21@gmail.com>
2025-11-02 00:55:32 -0400

    Implemented Mockmon switching

--- scripts/battle.gd
+++ scripts/battle.gd
@@ -3,22 +3,23 @@ extends Control
 @onready var player_trainer: CharacterBody2D = $Player
 @onready var npc_trainer: Node2D = $NpcTrainer
 
-@onready var battle_options: HBoxContainer = $BattleOptions
-@onready var switch_mockmon_box: MarginContainer = $SwitchMockmonBox
+@onready var battle_options: HBoxContainer = $UiBody/BattleOptions
+@onready var switch_mockmon_box: MarginContainer = $UiBody/SwitchMockmonBox
 
-@onready var player_mon_hp_bar: ProgressBar = $PlayerMonHpBar
-@onready var npc_mon_hp_bar: ProgressBar = $NpcMonHpBar
+@onready var player_mon_hp_bar: ProgressBar = $UiBody/PlayerMonHpBar
+@onready var npc_mon_hp_bar: ProgressBar = $UiBody/NpcMonHpBar
 
 @onready var player_mockmon_location: Marker2D = $PlayerMockmonLocation
 @onready var npc_mockmon_location: Marker2D = $NpcMockmonLocation
 
-@onready var mockmon_card_1: Button = $SwitchMockmonBox/VBoxContainer/HBoxContainer/MockmonCard1
-@onready var mockmon_card_2: Button = $SwitchMockmonBox/VBoxContainer/HBoxContainer/MockmonCard2
-@onready var mockmon_card_3: Button = $SwitchMockmonBox/VBoxContainer/HBoxContainer2/MockmonCard3
-@onready var mockmon_card_4: Button = $SwitchMockmonBox/VBoxContainer/HBoxContainer2/MockmonCard4
-@onready var mockmon_card_5: Button = $SwitchMockmonBox/VBoxContainer/HBoxContainer3/MockmonCard5
-@onready var mockmon_card_6: Button = $SwitchMockmonBox/VBoxContainer/HBoxContainer3/MockmonCard6
+@onready var mockmon_card_1: Button = $UiBody/SwitchMockmonBox/VBoxContainer/HBoxContainer/MockmonCard1
+@onready var mockmon_card_2: Button = $UiBody/SwitchMockmonBox/VBoxContainer/HBoxContainer/MockmonCard2
+@onready var mockmon_card_3: Button = $UiBody/SwitchMockmonBox/VBoxContainer/HBoxContainer2/MockmonCard3
+@onready var mockmon_card_4: Button = $UiBody/SwitchMockmonBox/VBoxContainer/HBoxContainer2/MockmonCard4
+@onready var mockmon_card_5: Button = $UiBody/SwitchMockmonBox/VBoxContainer/HBoxContainer3/MockmonCard5
+@onready var mockmon_card_6: Button = $UiBody/SwitchMockmonBox/VBoxContainer/HBoxContainer3/MockmonCard6
 
+@onready var dialog_label: RichTextLabel = $UiBody/BattleOptions/MarginContainer/DialogLabel
 
 var turn_count = 0; ## the turn count
 var battling = true;
@@ -26,8 +27,18 @@ var battling = true;
 var player_move_messages : Array[String] = []; ## messages that the game will display before a move is done.
 var player_move_message_index = 0;
 
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("next_dialog"):
+		next_dialog();
+
+func next_dialog(): ## progress to the next dialog if one is available
+	if player_move_messages.size() != 0: # removes and returns the first message in player_move_messages
+			dialog_label.text = player_move_messages.pop_front();
+
+
 func _ready() -> void:
-	battle_options.hide();
+	battle_options.show();
 	switch_mockmon_box.hide();
 	
 	var current_player_party = player_trainer.mockmon_party;
@@ -58,9 +69,16 @@ func battle(player: CharacterBody2D, npc: Node2D) -> void: ## starts a battle be
 	player_trainer.current_mockmon.global_position = player_mockmon_location.global_position;
 	npc_trainer.current_mockmon.global_position = npc_mockmon_location.global_position;
 
-	while battling:
+	while battling: # every round
+		player_trainer.current_mockmon.show(); # set the mockmon's locations
+		npc_trainer.current_mockmon.show();
+		player_trainer.current_mockmon.global_position = player_mockmon_location.global_position;
+		npc_trainer.current_mockmon.global_position = npc_mockmon_location.global_position;
+	
+		player_mon_hp_bar.value = player_trainer.current_mockmon.currentHp; # set hp bars
 		player_mon_hp_bar.value = player_trainer.current_mockmon.currentHp;
-		player_mon_hp_bar.value = player_trainer.current_mockmon.currentHp;
+		
+		next_dialog();
 		
 		# check which player's pokemon has the faster speed, if tied, pick random.
 		if npc.current_mockmon.BASE_SPEED > player.current_mockmon.BASE_SPEED:
@@ -97,9 +115,15 @@ func player_make_move():
 	
 
 func player_switch_mon(mon_team_number: int) -> void: ## switch the current mon from selected mon in party.
-	player_move_messages.append(player_trainer.current_mockmon + " return!");
-	player_trainer.current_mockmon = player_trainer.mockmon_party[mon_team_number-1];
-	player_move_messages.append("Go " + player_trainer.current_mockmon + "!");
+	if mon_team_number <= player_trainer.mockmon_party.size(): # only run if the inputted team number is valid given the team size
+		player_move_messages.append(player_trainer.current_mockmon.MOCKMON_NAME + " return!");
+		next_dialog();
+		player_trainer.current_mockmon.hide();
+		player_trainer.current_mockmon = player_trainer.mockmon_party[mon_team_number-1];
+		player_move_messages.append("Go " + player_trainer.current_mockmon.MOCKMON_NAME + "!");
+		next_dialog();
+		player_trainer.current_mockmon.show(); # set the mockmon's locations
+		player_trainer.current_mockmon.global_position = player_mockmon_location.global_position;
 	
 
 func _end_battle_player() -> void: ## player lost/quit the battle
@@ -138,3 +162,6 @@ func _on_mockmon_card_5_pressed() -> void:
 func _on_mockmon_card_6_pressed() -> void:
 	switch_mockmon_box.hide();
 	player_switch_mon(6);
+
+func _on_hide_switch_mockmon_pressed() -> void:
+	switch_mockmon_box.hide();
