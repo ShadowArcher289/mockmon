8597c6468239ba7ceeeca290a74bc74713d795e2
tn1207 <tn1207@g.rit.edu>
2025-11-02 09:27:37 -0500

    Sources
    Gave sources for character and overworld sprites

--- scripts/world.gd
+++ scripts/world.gd
@@ -1,5 +1,6 @@
 extends Node2D
-
+# Trainer & Overworld Sprites: https://www.spriters-resource.com/game_boy_advance/pokemonemerald/
+# https://eeveeexpo.com/essentials/
 func _ready() -> void:
 	await get_tree().create_timer(1).timeout;
 	get_tree().change_scene_to_file("res://scenes/battle.tscn");
